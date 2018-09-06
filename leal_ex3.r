9c1a28c899526d81a0f89130dc4e675926bd4100
TriziaLeal <mmleal@up.edu.ph>
2018-09-06 23:00:10 +0800

    Check for dynamic rows and columns

--- leal_ex3.r
+++ leal_ex3.r
@@ -4,46 +4,46 @@ AugCoeffMatrix <- function(system){
   else{
   rownames = NULL;
   colnames = NULL;
-  size = length(system);
-  i=1;
   
-  while (i <= size){
-    rownames = c(rownames,i);
-    colnames = c(colnames,paste("x",i,sep=""));
-    i = i + 1;
+  for (i in 1:length(system)){
+    rownames = c(rownames,i)
   }
-  
+  for (i in 1:length(getVariables(system))){
+    colnames = c(colnames,paste("x",sep="",i))
+  }
   colnames = c(colnames,"RHS")
 
   #instantiate matrix
-  m = matrix(0,nrow=size,ncol=size+1,dimnames = list(rownames,colnames), byrow = TRUE)
-  variables = substring(deparse(system[[1]])[1],11,nchar(deparse(system[[1]])[1])-2)
-
-  for (s in 1:length(system)){
-    equation_string = deparse(system[[s]])[2];
+  m = matrix(0,nrow=length(rownames),ncol=length(colnames),dimnames = list(rownames,colnames), byrow = TRUE)
+  for (row in 1:length(rownames)){
+    equation_string = deparse(system[[row]])[2];
     equation_term = strsplit(equation_string," + ",fixed=TRUE);
-    equation_term=list(strsplit(equation_term[[1]]," ",fixed=TRUE));
-
+    equation_term = list(strsplit(equation_term[[1]]," ",fixed=TRUE));
+    
+    #assign values of the matrix
     for (col in 1:length(colnames)){
       if(is.na(equation_term[[1]][[col]][3])){
-        m[s,"RHS"] = equation_term[[1]][[col]][1]
+        m[row,"RHS"] = equation_term[[1]][[col]][1]
       }
       else{
-      m[s,equation_term[[1]][[col]][3]] = equation_term[[1]][[col]][1]
+      m[row,equation_term[[1]][[col]][3]] = equation_term[[1]][[col]][1]
       }
     }
   }
   
-  return (list(augcoeffmatrix = m,variables = getVariables(system)))
+ return (list(augcoeffmatrix = m,variables = getVariables(system)))
   }
 }
 
+#gets variables of the functions
 getVariables <- function (system){
   var = deparse(system[[1]])[1];
   var = substring(var,11,nchar(var)-2)
   var = strsplit(var,", ",fixed=TRUE)
   return (var[[1]])
 }
+
+#checks validity of the functions
 isValid <- function (system) {
   numOfTerms = deparse(system[[1]])[2];
   numOfTerms = strsplit(numOfTerms," + ",fixed = TRUE)
@@ -52,21 +52,19 @@ isValid <- function (system) {
     temp = deparse(s)[2];
     temp = strsplit(temp," + ",fixed = TRUE)
     temp = length(temp[[1]])
-    print(temp)
     if (temp != numOfTerms)
       return (FALSE)
   }
   return (TRUE)
 }
 
-E1 <- function (x1, x2, x3) 0.3 * x1 + 10 * x3 + -71.4+ -0.2 * x2;
-E2 <- function (x1, x2, x3) 3 * x1 + -0.2 * x2 + -0.1 * x3 + -7.85;
-E3 <- function (x1, x2, x3) 0.1 * x1 + 7 * x2 + -0.3 * x3  + 19.3;
+E1 <- function (x1, x2, x3, x4) 11 * x1 + 13 * x3 + 14 + 12 * x2 + 1 * x4;
+E2 <- function (x1, x2, x3, x4) 21 * x1 + 22 * x2 + 23 * x3 + 24 + 2 * x4;
+E3 <- function (x1, x2, x3, x4) 31 * x1 + 32 * x2 + 33 * x3  + 34 + 3 * x4;
+E4 <- function (x1, x2, x3, x4) 41 * x1 + 42 * x2 + 43 * x3  + 44  + 4 * x4;
 
-system <- list(E1, E2, E3);
+system <- list(E1, E2, E3, E4);
 
 result = AugCoeffMatrix(system);
-#result
-#result$variables
-#result$augcoeffmatrix
-isValid(system)
+print(result)
+print(result$augcoeffmatrix)
